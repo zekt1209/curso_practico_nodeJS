97cee7590ba6067afe532134575edc9f0f7cd2f1
Vic-Laptop <victor_27edge@hotmail.com>
2023-12-29 22:16:25 -0600

    Clase 'Autenticación: gestión de permisos'
    - Creamos el archivo secure.js en el componende 'user' donde gestionamos los permiso, exportamos un middeware con
    las acciones que el usuario puede realizar como 'update', y es la accion que gestionaremos de momento para cuando
    quiera actualizar un usuario, validar que solo pueda actualizar info de su usuario y no de otros
    
    - En el archivo de config.js guardamos el secreto del token para mas seguridad
    
    - Modificamos el archivo index.js de la carpeta token, donde creamos funciones para validar el token, como:
    decodeHeader, getToken, verify, y creamos un objeto con una funcion que usa todas estas funciones para
    decodificar el token y es la que manda a llamar el archivo secure.js, entonces aqui comparamos que el
    usuario que quiere modificar, sea el mismo usuario del token

---
+++ api/components/user/secure.js
@@ -0,0 +1,30 @@
+// Aqui gestionamos los permisos
+// Confirmamos que el usuario que quiere realizar el movimiento, tiene permisos para realizar la transaccion, e.j, si el usuario quiere actalizar algun atributo de su perfil, comprobar que sea el, y que no pueda actualizar info de alguien mas
+
+const token = require("../../../token");
+
+// Exportamos un middleware
+module.exports = function checkAuth(action) { // Action -> Accion que queremos ejecutar (la usaremos en el middleware)
+
+    function middleware(req, res, next) {
+        // Posibles acciones que pueden suceder
+        // Gestionamos los permisos
+        switch(action) {
+            case 'update':
+                // Lo que se tiene que cumplir para que el user pueda hacer un update
+                
+                // id del user que hace la peticion
+                const owner = req.body.id;
+
+                // Funcion que valida que el que hace la peticion, es el mismo que esta registrado en la DB de auth
+                token.check.own(req, owner);
+                break;
+
+            default:
+                next();
+        }
+    }
+
+
+    return middleware;
+};
--- token/index.js
+++ token/index.js
@@ -1,8 +1,67 @@
 const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const secret = config.jwt.secret;
 
 function sign(data) {
-    return jwt.sign(data, 'secreto');
+    return jwt.sign(data, secret);
+};
+
+function verify(token) {
+    return jwt.verify(token, secret);
 }
+
+// ---------------------------------------------
+// Gestion de permisos
+
+
+
+const check = {
+    own: function(req, owner) {
+        // Aqui es dondevamos a hacer toda la compobacion
+        // 1- Decodificar el token
+        const decoded = decodeHeader(req);
+        console.log(decoded);
+    },
+}
+
+function getToken(auth) {
+    // Bearer kslhjndioadhieohjioejniewo
+
+    
+    // Asegurarnos de que venga un header o no
+    if (!auth) {
+        throw new Error('No viene token');
+    }
+
+    // Asegurarnos que el formato del token sea correcto 'Bearer token'
+    if (auth.indexOf('Bearer ') == -1) {
+        throw new Error('Formato de token invalido');
+    }
+    
+    // - Tenemos que quitar el 'Bearer '
+    let token = auth.replace('Bearer ', '');
+
+
+}
+
+// 1- Decodificar el token
+function decodeHeader(req) { // Una vez tengamos la request, generaremos el token decodificado
+    // El header que queremos recibir
+    const authorization = req.headers.authorization || '';
+
+    // Sacar el token desde el tipo de header que venga
+    const token = getToken(authorization);
+
+    // Ejecutar funcion que verifique que el token es valido
+    const decoded = verify(token);
+
+    // Lo dejamos en la request por si queremos utilizar esta info mas adelante
+    req.user = decoded;
+
+    return decoded;
+}
+
 
 module.exports = {
     sign,
