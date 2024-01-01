80ffef689dc1232c98281bc78a8ad237509a8b10
Vic-Laptop <victor_27edge@hotmail.com>
2024-01-01 13:42:04 -0600

    Clase 'Completando la base de datos'
    - Agregamos los metodos faltantes de la base de datos (get, insert, update, query)
    - Agregamos la tabla auth en la base de datos phpMyAdmin
    
    FALTA
    - Hacer el metodo 'remove'
    - El metodo insert no funciona: Tal como está el código, siempre viene información dentro del data.id (si es registro
      nuevo el data.id lo estamos llenando desde el controller con el id generado por nanoid).
    
      Como desde el controlador para cualquier caso data.id viene lleno, entonces se está entrando directamente al update
      y nunca en el insert.

--- storage/mysql.js
+++ storage/mysql.js
@@ -52,6 +52,62 @@ function list(table) {
     })
 }
 
+function get(table, id) {
+
+    return new Promise( (resolve, reject) => {
+        connection.query(`SELECT * FROM ${table} WHERE id = ${id}`, (err, data) => {
+            if (err) return reject(err);
+            resolve(data);
+        });
+    } );
+
+}
+
+function insert(table, data) {
+
+    return new Promise( (resolve, reject) => {
+        connection.query(`INSERT INTO ${table} SET ?`, data, (err, result) => {
+            if (err) return reject(err);
+            resolve(result);
+        })
+    } )
+
+}
+
+function update(table, data) {
+
+    return new Promise( (resolve, reject) => {
+        connection.query(`UPDATE ${table} SET ? WHERE id=?`, [data, data.id], (err, result) => {
+            if (err) return reject(err);
+            resolve(result);
+        })
+    } )
+
+}
+
+function upsert(table, data) {
+    if (data && data.id) {
+        return update(table, data);
+    } else {
+        return insert(table, data);
+    }
+}
+
+function query(table, query) {
+
+    return new Promise( (resolve, reject) => {
+        connection.query(`SELECT * FROM ${table} WHERE ?`, query, (err, res) => {
+            if (err) return reject(err);
+            resolve(res[0] || null);
+        })
+    } );
+
+};
+
+function remove(table, id) {
+
+}
+
 /* async function list(table) {
 
         const data = await connection.query(`SELECT * FROM ${table}`);
@@ -61,4 +117,8 @@ function list(table) {
 
 module.exports = {
     list,
+    get,
+    upsert,
+    query,
+    remove,
 }
