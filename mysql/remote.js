404ce35a340c40688d8a0ba6360ebc44c97e6032
Vic-Laptop <victor_27edge@hotmail.com>
2024-01-17 01:39:16 -0600

    lil change

--- mysql/remote.js
+++ mysql/remote.js
@@ -4,11 +4,37 @@ function createRemoteDB(host, port) {
     const URL = 'http://' +host+ ':' +port;
 
     function list(table) {
-        return request('GET', table);
+        return req('GET', table);
     }
     
     // function get(table, id) {}
     // function upsert(table, data) {}
     // function query(table, query, join) {}
+
+    function req(method, table, data) {
+        let url = URL +'/'+ table;
+        body = '';
+
+        return new Promise((resolve, reject) => {
+            request({
+                method,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                url,
+                body,
+            }, (err, req, body) => {
+                if (err) {
+                    console.error('Error en la funcion req de la base de datos remota', err);
+                    return reject(err.message);
+                }
+
+                const resp = JSON.parse(body);
+                return resolve(resp.body);
+            })
+        })
+    }
     
 }
+
+module.exports = createRemoteDB;
